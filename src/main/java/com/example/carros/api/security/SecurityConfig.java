c614b7cf2094c2b6a95536ff04e99fd34f6a7ae1
DavidTM <deivi_gf144@hotmail.com>
2021-03-15 14:00:47 -0400

    add userDetailsService

--- src/main/java/com/example/carros/api/security/SecurityConfig.java
+++ src/main/java/com/example/carros/api/security/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.example.carros.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    @Qualifier("userDetailsService")
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,10 +35,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-            auth
-                .inMemoryAuthentication().passwordEncoder(encoder)
-                .withUser("user").password(encoder.encode("user")).roles("USER")
-                .and()
-                .withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
+            auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+
+//                .inMemoryAuthentication().passwordEncoder(encoder)
+//                .withUser("user").password(encoder.encode("user")).roles("USER")
+//                .and()
+//                .withUser("admin").password(encoder.encode("admin")).roles("USER", "ADMIN");
     }
 }
